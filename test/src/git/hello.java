eeaa5cfc4500cef40b8698a59bb680d4bda05c48
chenCY <echoccy@163.com>
2022-07-03 21:22:45 +0800

    分支第1次提交

--- test/src/git/hello.java
+++ test/src/git/hello.java
@@ -4,6 +4,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("jjjjj");
         System.out.println("你写的好垃圾哦");
-        System.out.println("jjjjj");
+        System.out.println("第二个分支写的代码");
     }
 }
