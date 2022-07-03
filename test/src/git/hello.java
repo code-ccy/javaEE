4083aa77a2ad256d40764355796a0bca566f9396
chenCY <echoccy@163.com>
2022-07-03 21:21:26 +0800

    第2次提交

--- test/src/git/hello.java
+++ test/src/git/hello.java
@@ -3,5 +3,7 @@ package git;
 public class hello {
     public static void main(String[] args) {
         System.out.println("jjjjj");
+        System.out.println("你写的好垃圾哦");
+        System.out.println("jjjjj");
     }
 }
